b0bc78c823c26a139d5425ab4677636ef9acf6b1
AyazAktaş <ayazaktastr@gmail.com>
2024-07-28 13:18:48 +0300

    ürün detay sayfasında paylaş ve sepet resimleri için snackbar ve nagivation gecis sağlandı

--- app/src/main/java/com/example/trendyolapp/ui/fragment/UrunDetayFragment.kt
+++ app/src/main/java/com/example/trendyolapp/ui/fragment/UrunDetayFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.trendyolapp.R
 import com.example.trendyolapp.databinding.FragmentUrunDetayBinding
+import com.google.android.material.snackbar.Snackbar
 
 class UrunDetayFragment : Fragment() {
     private lateinit var binding: FragmentUrunDetayBinding
@@ -20,6 +22,12 @@ class UrunDetayFragment : Fragment() {
         binding.imageViewUrun.setImageResource(resources.getIdentifier(urun.urunResim,"drawable",requireContext().packageName))
         binding.textViewAciklama.text=urun.urunAciklama
         binding.textViewfiyat.text="${urun.urunFiyat} TL"
+        binding.imageViewMesajlar.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.sepeteGecis)
+        }
+        binding.imageViewBildirimler.setOnClickListener {
+            Snackbar.make(it,"Ürün bağlantısı kopyalandı.",Snackbar.LENGTH_SHORT).show()
+        }
 
         return binding.root
 

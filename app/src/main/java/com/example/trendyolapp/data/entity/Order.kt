2affe1e5c1af30cb5b7824740ee2d43e07e75ae7
AyazAktaş <ayazaktastr@gmail.com>
2024-08-09 12:41:32 +0300

    siparis card view

--- app/src/main/java/com/example/trendyolapp/data/entity/Order.kt
+++ app/src/main/java/com/example/trendyolapp/data/entity/Order.kt
@@ -3,4 +3,5 @@ package com.example.trendyolapp.data.entity
 data class Order(val orderCode: String,
                  val date: String,
                  val time: String,
+
                  val products: List<Urun>)
